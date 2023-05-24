5d7a788815ae9deea0ae055374798725c4e8e463
BrasseurTristan <brasseur.tristan@gmail.com>
2023-05-24 22:27:17 +0200

    Add new button and add picture

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,161 +17,234 @@ function dayAndHour(){
 function addZero(number){
     return number < 10 ? '0' + number : number
 }
-window.onload = () => {setInterval('dayAndHour()',5000);
+window.onload = () => {setInterval('dayAndHour()',1000);
 };
 
 /*****************************************************************************/
 /*********************************NEW WINDOW**********************************/
 /*****************************************************************************/
 
-const buttonDiv = document.querySelector(".testclick");
-const buttonDiv2 = document.querySelector(".testclick2");
+const descButton = document.getElementById('description');
+const resButton = document.getElementById('resume');
+const skillButton = document.getElementById('skill');
+const languageSkillButton = document.getElementById('languageSkill');
+const studyButton = document.getElementById('formation');
+const hobbyButton = document.getElementById('hobby');
+const pictureButton = document.getElementById('image');
+const headerHeight = document.getElementById("header").offsetHeight;
 let X;
 let Y;
 let mouse_down = false;
 let topDiv = null;
-let exist=false;
+let isFullscreen = false;
+let zIndexCounter = 100;
 
+function createWindow(title,text,button) {
+  
+  const mainWindow = document.createElement('div');
+  mainWindow.classList.add('body-window');
+  document.body.appendChild(mainWindow);
+  const headerWindow = document.createElement('div');
+  headerWindow.classList.add('header-window');
+  mainWindow.appendChild(headerWindow);
+  const titleWindow = document.createElement('h3');
+  titleWindow.classList.add('title');
+  titleWindow.innerHTML = title;
+  headerWindow.appendChild(titleWindow);
+  const reduceButton = document.createElement('div');
+  reduceButton.classList.add('button','reduce');
+  headerWindow.appendChild(reduceButton);
+  const upscaleButton = document.createElement('div');
+  upscaleButton.classList.add('button','upscale');
+  headerWindow.appendChild(upscaleButton);
+  const closeButton = document.createElement('div');
+  closeButton.classList.add('button','close');
+  headerWindow.appendChild(closeButton);
+  const content = document.createElement('div');
+  content.classList.add('content');
+  mainWindow.appendChild(content);
+  content.innerHTML = text;
+  mainWindow.style.zIndex = zIndexCounter;
+  zIndexCounter++;
 
-
-function createWindow(title,text,divId) {
-  
-    const mainWindow = document.createElement('div');
-    mainWindow.classList.add('body-window');
-    document.body.appendChild(mainWindow);
-    const headerWindow = document.createElement('div');
-    headerWindow.classList.add('header-window');
-    mainWindow.appendChild(headerWindow);
-    const titleWindow = document.createElement('h3');
-    titleWindow.classList.add('title');
-    titleWindow.innerHTML = title;
-    headerWindow.appendChild(titleWindow);
-    const reduceButton = document.createElement('div');
-    reduceButton.classList.add('button','reduce');
-    headerWindow.appendChild(reduceButton);
-    const upscaleButton = document.createElement('div');
-    upscaleButton.classList.add('button','upscale');
-    headerWindow.appendChild(upscaleButton);
-    const closeButton = document.createElement('div');
-    closeButton.classList.add('button','close');
-    headerWindow.appendChild(closeButton);
-    const content = document.createElement('div');
-    content.classList.add('content');
-    mainWindow.appendChild(content);
-    content.innerHTML = text;
-    // Permet de mettre en avant une div avec le click
-    mainWindow.addEventListener('click', bringToFront);
-  
-    function bringToFront() {
-      if (topDiv) {
-        topDiv.style.zIndex = '0'; // Mettre la div précédente à une valeur inférieure
-      }
-    
-      this.style.zIndex = '9999';
-      topDiv = this; // Mettre à jour la div actuellement en premier plan
-    }
-  
   
     headerWindow.onmousedown = function(event) {
-  
-      let shiftX = event.clientX - headerWindow.getBoundingClientRect().left;
-      let shiftY = event.clientY - headerWindow.getBoundingClientRect().top;
-  
-      moveAt(event.pageX, event.pageY);
-  
-      function moveAt(pageX, pageY) {
-          // Obtenir la largeur et la hauteur de la fenêtre
-          const windowWidth = document.documentElement.clientWidth;
-          const windowHeight = document.documentElement.clientHeight;
-  
-          // Obtenir la largeur et la hauteur de la boîte
-          const boxWidth = mainWindow.offsetWidth;
-          const boxHeight = mainWindow.offsetHeight;
-  
-          // Calculer les limites de la boîte
-          const minX = 0;
-          const minY = 0;
-          const maxX = windowWidth - boxWidth;
-          const maxY = windowHeight - boxHeight;
-  
-          // Restreindre les coordonnées X et Y pour qu'elles ne dépassent pas les limites de la fenêtre
-          const restrictedX = Math.min(Math.max(minX, pageX - shiftX), maxX);
-          const restrictedY = Math.min(Math.max(minY, pageY - shiftY), maxY);
-  
-          // Appliquer les coordonnées restreintes à la boîte
-          mainWindow.style.left = restrictedX + 'px';
-          mainWindow.style.top = restrictedY + 'px';
+      if(isFullscreen){
+        return
       }
-  
-      function onMouseMove(event) {
-        moveAt(event.pageX, event.pageY);
-      }
-  
-      document.addEventListener('mousemove', onMouseMove);
-  
-      document.onmouseup = function() {
-        headerWindow.onmouseup = null;
-        document.removeEventListener('mousemove', onMouseMove);
-      };
+    // Calculer la différence entre la position du clic de souris et le coin supérieur gauche de headerWindow
+    let shiftX = event.clientX - headerWindow.getBoundingClientRect().left;
+    let shiftY = event.clientY - headerWindow.getBoundingClientRect().top;
+    // Définir la fonction de rappel pour l'événement mousemove sur tout le document
+    document.onmousemove = function(event) {
+      // Obtenir la largeur et la hauteur de la fenêtre
+      const windowWidth = document.documentElement.clientWidth;
+      const windowHeight = document.documentElement.clientHeight;
+      // Obtenir la largeur et la hauteur de la boîte principale
+      const boxWidth = mainWindow.offsetWidth;
+      const boxHeight = mainWindow.offsetHeight;
+      // Définir les limites de déplacement de la boîte principale
+      const minX = 0;
+      const minY = document.getElementById("header").offsetHeight; // Utiliser la hauteur de la balise avec l'ID "header"
+      const maxX = windowWidth - boxWidth;
+      const maxY = windowHeight - boxHeight;
+      // Calculer les coordonnées restreintes de la boîte principale en fonction de la position de la souris
+      const restrictedX = Math.min(Math.max(minX, event.pageX - shiftX), maxX);
+      const restrictedY = Math.min(Math.max(minY, event.pageY - shiftY), maxY);
+      // Appliquer les coordonnées restreintes à la boîte principale pour la déplacer
+      mainWindow.style.left = restrictedX + 'px';
+      mainWindow.style.top = restrictedY + 'px';
     };
   
-    closeButton.addEventListener('click', function() {
-      mainWindow.parentNode.removeChild(mainWindow);
-     
+    // Définir la fonction de rappel pour l'événement mouseup sur tout le document
+    document.onmouseup = function() {
+      // Supprimer la fonction de rappel pour l'événement mousemove
+      document.onmousemove = null;
+      // Supprimer la fonction de rappel pour l'événement mouseup
+      document.onmouseup = null;
+    };
+  };
   
-      
-    });
   
-    function fullscreen(currentWidth,currentHeight,currentTop,currentLeft){
-  
-      if(mainWindow.classList.contains('fullscreen')){
-        mainWindow.style.left =  '0px';
-        mainWindow.style.top = '0px';
-        mainWindow.style.width =  '100%';
-        mainWindow.style.height = '100%';
-      }else {
-        mainWindow.style.left =  currentLeft;
-        mainWindow.style.top = currentTop;
-        mainWindow.style.width =  currentWidth;
-        mainWindow.style.height = currentHeight;
-      }
+
+
+
+  const paramStyles = window.getComputedStyle(mainWindow);
+  console.log(paramStyles);
+  let currentWidth = paramStyles.getPropertyValue('width');
+  let currentHeight = paramStyles.getPropertyValue('height');
+  let currentTop = paramStyles.getPropertyValue('top');
+  let currentLeft = paramStyles.getPropertyValue('left');
+
+  function fullscreen(currentWidth,currentHeight,currentTop,currentLeft){
+    
+    if(mainWindow.classList.contains('fullscreen')){
+      mainWindow.style.left =  '0px';
+      mainWindow.style.top =  headerHeight + 'px';
+      mainWindow.style.width =  '100%';
+      mainWindow.style.height = '100%';
+      isFullscreen = true;
+    }else {
+      mainWindow.style.left =  currentLeft;
+      mainWindow.style.top = currentTop;
+      mainWindow.style.width =  currentWidth;
+      mainWindow.style.height = currentHeight;
+      isFullscreen = false;
     }
-    const paramStyles = window.getComputedStyle(mainWindow);
-    let currentWidth = paramStyles.getPropertyValue('width');
-    let currentHeight = paramStyles.getPropertyValue('height');
-    let currentTop = paramStyles.getPropertyValue('top');
-    let currentLeft = paramStyles.getPropertyValue('left');
-    
-  
-    upscaleButton.addEventListener('click', function(){
-      
-      if(!mainWindow.classList.contains('fullscreen')){
-        currentHeight = paramStyles.getPropertyValue('height');
-        currentWidth = paramStyles.getPropertyValue('width');
-        currentTop = paramStyles.getPropertyValue('top');
-        currentLeft = paramStyles.getPropertyValue('left');
-      }
-  
-      mainWindow.classList.toggle('fullscreen');
-      fullscreen(currentWidth,currentHeight,currentTop,currentLeft)
-    
-      
-    });
-    // exist = true;
   }
 
-buttonDiv.addEventListener('dblclick',()=>{
-
-    title = 'Description.txt';
-    text = '<p> Passionné d\'informatique, j\'ai décidé de faire une reconversion professionnelle en tant que développeur web. Je suis à la recherche d\'un stage du 26 juin jusqu\’au 28 août 2023. <br>Je suis également à la recherche d\'une opportunité d\'alternance pour poursuivre mes études et obtenir un diplôme de développeur FullStack. Le rythme de l\'alternance sera d\'une semaine en formation pour trois semaines en entreprise.</p>';
-    // exist ? "" :
-     createWindow(title,text);
-
   
+  upscaleButton.addEventListener('click', function(){
+    
+    if(!mainWindow.classList.contains('fullscreen')){
+      currentHeight = paramStyles.getPropertyValue('height');
+      currentWidth = paramStyles.getPropertyValue('width');
+      currentTop = paramStyles.getPropertyValue('top');
+      currentLeft = paramStyles.getPropertyValue('left');
+      mainWindow.classList.add('fullscreen');
+    }else{
+      mainWindow.classList.remove('fullscreen');
+
+    }
+    fullscreen(currentWidth,currentHeight,currentTop,currentLeft);
+  });
+  
+  mainWindow.addEventListener('click', bringToFront);
+
+  function bringToFront() {
+    const currentZIndex = parseInt(this.style.zIndex);
+    if (currentZIndex === zIndexCounter - 1) {
+      return; // Ne rien faire si la fenêtre est déjà en premier plan
+    }
+    if (topDiv) {
+      topDiv.style.zIndex = '0'; // Mettre la div précédente à une valeur inférieure
+    }
+    this.style.zIndex = '9999';
+    topDiv = this; // Mettre à jour la div actuellement en premier plan
+  }
+  
+  closeButton.addEventListener('click', function() {
+    mainWindow.parentNode.removeChild(mainWindow);
+    enableButton(button);    
+  
+  });
+  
+}  
+
+function enableButton(button) {
+  button.classList.remove('disabled');
+}
+
+descButton.addEventListener('dblclick',function(){
+  if (!this.classList.contains('disabled')) {
+    title = 'Description.txt';
+    text = '<p> Passionné d\'informatique, j\'ai décidé de faire une reconversion professionnelle en tant que développeur web. Je suis à la recherche d\'un stage du 26 juin jusqu\’au 28 août 2023. </br></br>Je suis également à la recherche d\'une opportunité d\'alternance pour poursuivre mes études et obtenir un diplôme de développeur FullStack. Le rythme de l\'alternance sera d\'une semaine en formation pour trois semaines en entreprise.</p>';
+    createWindow(title,text,this);  
+    this.classList.add('disabled');
+  }
+     
 });
-buttonDiv2.addEventListener('dblclick',()=>{
+
+resButton.addEventListener('dblclick',function(){
+  if (!this.classList.contains('disabled')) {
     title = 'Résumé-de-carrière.txt';  
-    text = '<h3> Sidérurgie</h3><ul><li>Ouvrier sidérurgique, pontier cabine, pontier sol, cariste - ARCELOR MITTAL -Juin 2015 à Mars 2022  </li></ul><h3>Automobile</h3><ul><li> Agent de fabrication - SOVAB, Batilly - Décembre 2014 à Avril 2015 </li></ul><h3>Informatique</h3><ul><li>Assistant technicien informatique - VANKSEN, Luxembourg - Novembre 2014</li></ul><h3> Restauration</h3><ul><li>Aide cuisinier - SERVIOR, Dudelange - Décembre 2013 à Septembre 2014</li><li>Fabrication et vente de pâtes fraiches - PASTA FRESCA, Luxembourg - 2013</li></ul>';
-    // exist ? "" : 
-    createWindow(title,text);
+    text = '<h3> Sidérurgie</h3></br><ul><li>Ouvrier sidérurgique, pontier cabine, pontier sol, cariste - ARCELOR MITTAL -Juin 2015 à Mars 2022  </li></ul></br><h3>Automobile</h3></br><ul><li> Agent de fabrication - SOVAB, Batilly - Décembre 2014 à Avril 2015 </li></ul></br><h3>Informatique</h3></br><ul><li>Assistant technicien informatique - VANKSEN, Luxembourg - Novembre 2014</li></ul></br><h3> Restauration</h3></br><ul><li>Aide cuisinier - SERVIOR, Dudelange - Décembre 2013 à Septembre 2014</li><li>Fabrication et vente de pâtes fraiches - PASTA FRESCA, Luxembourg - 2013</li></ul>';
+    createWindow(title,text,this);
+    this.classList.add('disabled');
+  }
 });
+
+skillButton.addEventListener('dblclick',function(){
+  if (!this.classList.contains('disabled')) {
+    title = 'Compétences.txt';  
+    text = '<ul><li>Prise de décision</li><li>Fléxibilité</li><li>Combativité</li><li>Organisation</li><li>Autonomie</li><li>Ecoute</li><li>Persévérance</li></ul>';
+    createWindow(title,text,this);
+    this.classList.add('disabled');
+  }
+});
+
+studyButton.addEventListener('dblclick',function(){
+  if (!this.classList.contains('disabled')) {
+    title = 'Formations.txt';  
+    text = '<h3>Développeur Web et Web mobile</h3></br><ul><li>Titre RNCP de niveau 6 - Développeur FullStack - à partir d\'Octobre 2023</li><li>Titre RNCP de niveau 5 - Développeur Web - Décembre 2022 - Octobre 2023</li><li>Remise à niveau des métiers du numérique - Septembre - Décembre 2022</li></br><h3>Technique du froid et du conditionnement de l\'air</h3></br><li>Baccalauréat professionnel - 2012 </li><li>Brevet d\'étude professionnel - 2010</li></br><h3>Certification</h3></br><li>MOOC de l\'ANSSI - Avril 2023</li>';
+    createWindow(title,text,this);
+    this.classList.add('disabled');
+  }
+});
+
+hobbyButton.addEventListener('dblclick',function(){
+  if (!this.classList.contains('disabled')) {
+    title = 'Loisirs.txt';  
+    text = '<ul><li>Informatique</li><li>Impression_3D</li><li>Longboard</li><li>Clavier custom</li></ul>';
+    createWindow(title,text,this);
+    this.classList.add('disabled');
+  }
+});
+
+languageSkillButton.addEventListener('dblclick',function(){
+  if (!this.classList.contains('disabled')) {
+    title = 'Skills.txt';  
+    text = '<h3>Skills :</h3></br><ul><li>HTML (acquis)</li><li>CSS (acquis)</li><li>Javascript (en cours)</li><li>PHP (en cours)</li><li>REACT (en cours)</li><li>ANGULAR (en cours)</li><li>SYMPHONY (en cours)</li><li>SQL (en cours)</li><li>Wordpress (acquis)</li><li>GIT (en cours)</li></ul>';
+    createWindow(title,text,this);
+    this.classList.add('disabled');
+  }
+});
+
+pictureButton.addEventListener('dblclick',function(){
+  if (!this.classList.contains('disabled')) {
+    title = 'Photo.jpg';  
+    text = '<img src="./assets/images/Photo_profil.png" id="picture" alt="Photo.png" title="Photo.png">';
+    createWindow(title,text,this);
+    const mainWindow = document.querySelector('.body-window');
+    const content = mainWindow.querySelector('.content');
+    content.classList.add('content-image');
+    this.classList.add('disabled');
+  }
+});
+
+
+
+
+
+    
+  
